af89e0434fbdb8166fedb065b923ed98c52cb93b
Thomas <degris@ualberta.ca>
2011-05-03 12:01:44 -0600

    More information on the node selected

--- zephyr.plugin.core/src/zephyr/plugin/core/internal/treeview/SelectionTreeListener.java
+++ zephyr.plugin.core/src/zephyr/plugin/core/internal/treeview/SelectionTreeListener.java
@@ -5,6 +5,7 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.TreeItem;
 
 import zephyr.ZephyrCore;
+import zephyr.plugin.core.api.codeparser.codetree.ClassNode;
 import zephyr.plugin.core.api.codeparser.interfaces.CodeNode;
 
 public class SelectionTreeListener implements SelectionListener {
@@ -15,7 +16,22 @@ public class SelectionTreeListener implements SelectionListener {
       return;
     CodeNode codeNode = (CodeNode) treeItem.getData();
     if (codeNode != null)
-      ZephyrCore.sendStatusBarMessage(codeNode.label());
+      ZephyrCore.sendStatusBarMessage(buildDescriptiveLabel(codeNode));
+  }
+
+  protected String buildDescriptiveLabel(CodeNode codeNode) {
+    StringBuilder label = new StringBuilder(codeNode.path());
+    if (codeNode instanceof ClassNode) {
+      String className = ((ClassNode) codeNode).instance().getClass().getSimpleName();
+      appendSuffix(label, className);
+    }
+    return label.toString();
+  }
+
+  protected void appendSuffix(StringBuilder label, String suffix) {
+    label.append("[");
+    label.append(suffix);
+    label.append("]");
   }
 
   @Override
