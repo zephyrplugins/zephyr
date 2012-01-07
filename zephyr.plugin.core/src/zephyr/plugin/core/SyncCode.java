e04ac900945385c8dec6b9a5a99c932a4629f850
Thomas <degris@ualberta.ca>
2012-01-07 11:18:33 +0100

    Save what is displayed in ClassViewProvider

--- zephyr.plugin.core/src/zephyr/plugin/core/SyncCode.java
+++ zephyr.plugin.core/src/zephyr/plugin/core/SyncCode.java
@@ -9,6 +9,7 @@ import zephyr.ZephyrCore;
 import zephyr.plugin.core.api.codeparser.codetree.ClockNode;
 import zephyr.plugin.core.api.codeparser.interfaces.CodeNode;
 import zephyr.plugin.core.api.codeparser.interfaces.CodeParser;
+import zephyr.plugin.core.api.codeparser.interfaces.ParentNode;
 import zephyr.plugin.core.api.codeparser.parsers.CodeTreeParser;
 import zephyr.plugin.core.api.monitoring.abstracts.MonitoredDataTraverser;
 import zephyr.plugin.core.api.synchronization.Clock;
@@ -43,5 +44,25 @@ public class SyncCode {
   public void removeClockNode(Clock clock) {
     ClockNode clockNode = clockNodes.remove(clock);
     ZephyrCore.busEvent().dispatch(new CodeStructureEvent(CodeStructureEvent.RemovedID, clockNode, clockNode));
+  }
+
+  public CodeNode findNode(String[] loadedPath) {
+    ClockNode selectedClockNode = null;
+    for (ClockNode clockNode : clockNodes.values())
+      if (clockNode.label().equals(loadedPath[0])) {
+        selectedClockNode = clockNode;
+        break;
+      }
+    if (loadedPath.length == 1)
+      return selectedClockNode;
+    CodeNode currentNode = selectedClockNode;
+    for (int i = 1; i < loadedPath.length; i++) {
+      if (currentNode == null)
+        return null;
+      if (!(currentNode instanceof ParentNode))
+        return null;
+      currentNode = ((ParentNode) currentNode).getChild(loadedPath[i]);
+    }
+    return currentNode;
   }
 }
--- zephyr.plugin.core/src/zephyr/plugin/core/helpers/InstanceManager.java
+++ zephyr.plugin.core/src/zephyr/plugin/core/helpers/InstanceManager.java
@@ -1,5 +1,8 @@
 package zephyr.plugin.core.helpers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.ui.IMemento;
 
 import zephyr.ZephyrCore;
@@ -9,11 +12,17 @@ import zephyr.plugin.core.api.codeparser.codetree.CodeTrees;
 import zephyr.plugin.core.api.codeparser.interfaces.CodeNode;
 import zephyr.plugin.core.api.synchronization.Clock;
 import zephyr.plugin.core.async.events.CastedEventListener;
+import zephyr.plugin.core.async.events.Event;
+import zephyr.plugin.core.async.listeners.EventListener;
 import zephyr.plugin.core.events.ClockEvent;
+import zephyr.plugin.core.events.CodeStructureEvent;
 import zephyr.plugin.core.internal.ZephyrPluginCore;
 import zephyr.plugin.core.views.SyncView;
 
 public class InstanceManager<T> {
+  private static final String PathRootType = "codetreelink";
+  private static final String PathLabelType = "nodelabel";
+
   public interface InstanceListener<T> extends SyncView {
     void onInstanceSet();
 
@@ -24,6 +33,13 @@ public class InstanceManager<T> {
   Clock clock;
   private CodeNode codeNode;
   private final InstanceListener<T> view;
+  private String[] loadedPath;
+  private final EventListener treeLoadedListener = new EventListener() {
+    @Override
+    public void listen(Event eventInfo) {
+      setCodetree();
+    }
+  };
 
   public InstanceManager(InstanceListener<T> view) {
     this.view = view;
@@ -34,6 +50,16 @@ public class InstanceManager<T> {
           unset();
       }
     });
+    ZephyrCore.busEvent().register(CodeStructureEvent.ParsedID, treeLoadedListener);
+  }
+
+  protected void setCodetree() {
+    if (codeNode != null || loadedPath == null)
+      return;
+    CodeNode loadedCodenode = ZephyrPluginCore.syncCode().findNode(loadedPath);
+    if (loadedCodenode == null)
+      return;
+    set(loadedCodenode);
   }
 
   public CodeNode codeNode() {
@@ -96,8 +122,29 @@ public class InstanceManager<T> {
   }
 
   public void parseMemento(IMemento memento) {
+    if (memento == null)
+      return;
+    IMemento pathRoot = memento.getChild(PathRootType);
+    if (pathRoot == null)
+      return;
+    List<String> pathList = new ArrayList<String>();
+    for (IMemento child : pathRoot.getChildren(PathLabelType))
+      pathList.add(child.getID());
+    loadedPath = new String[pathList.size()];
+    pathList.toArray(loadedPath);
+    setCodetree();
   }
 
   public void saveState(IMemento memento) {
+    String[] savedPath = codeNode != null ? codeNode.path() : loadedPath;
+    if (savedPath == null)
+      return;
+    IMemento pathRoot = memento.createChild(PathRootType);
+    for (String label : savedPath)
+      pathRoot.createChild(PathLabelType, label);
+  }
+
+  public void dispose() {
+    ZephyrCore.busEvent().unregister(CodeStructureEvent.ParsedID, treeLoadedListener);
   }
 }
